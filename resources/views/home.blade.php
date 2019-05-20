5aea1c7e24f1492fb3260bdaae63b9cf75035056
403228075@qq.com <403228075@qq.com>
2019-05-20 12:54:29 +0800

    首页调整

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="card-deck">
-        @foreach($article as $articles)
-        <div class="card" onclick="window.location.href='article/'+'{{$articles->id}}';">
-            <img class="card-img-top" src="{{ asset('uploads/'.$articles->picture) }}" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">{{ $articles->title }}</h5>
-                <p class="card-text">{{$articles->body}}</p>
-                <p class="card-text"><small class="text-muted">{{$articles->created_at->diffForHumans()}}</small></p>
-            </div>
-        </div>
+    <div class="container">
+        <div class="row">
+            @foreach($article as $articles)
+                <div class="col-md-4" onclick="window.location.href='article/'+'{{$articles->id}}';">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="{{ asset('uploads/'.$articles->picture) }}" data-holder-rendered="true">
+                        <div class="card-body">
+                            <p class="card-text">{{$articles->body}}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">去看看</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">免费</button>
+                                </div>
+                                <small class="text-muted">{{$articles->created_at->diffForHumans()}}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @endforeach
+        </div>
     </div>
-</div>
 @endsection
